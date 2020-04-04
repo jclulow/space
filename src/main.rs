c15b3f8c9fac52823bae89fe319e5c31907e12c8
Joshua M. Clulow <josh@sysmgr.org>
2020-04-04 06:17:14 +0000

    overwrite and redraw rather than clear screen

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ struct Space<'a> {
     settings: Settings,
     objects: Vec<Object>,
     term: ANSITerm<'a>,
-    scalemax: i32,
+    scalemax: f64,
     traces: RefCell<Vec<Trace>>,
 
     xmax: i32,
@@ -60,124 +60,127 @@ struct Space<'a> {
 }
 
 impl Space<'_> {
-    fn xw(&self) -> i32 {
-        self.xmax - self.xmin
+    fn xw(&self) -> f64 {
+        (self.xmax - self.xmin) as f64
     }
 
-    fn yw(&self) -> i32 {
-        self.ymax - self.ymin
+    fn yw(&self) -> f64 {
+        (self.ymax - self.ymin) as f64
     }
 
-    fn xc(&self) -> i32 {
-        self.xw() / 2 + self.xmin
+    fn xc(&self) -> f64 {
+        self.xw() / 2.0 + self.xmin as f64
     }
 
-    fn yc(&self) -> i32 {
-        self.yw() / 2 + self.ymin
+    fn yc(&self) -> f64 {
+        self.yw() / 2.0 + self.ymin as f64
     }
 
-    fn ysp(&self) -> i32 {
-        ((self.scalemax as f64) * 1.3_f64).round() as i32
+    fn ysp(&self) -> f64 {
+        self.scalemax * 1.3_f64
     }
 
-    fn xsp(&self) -> i32 {
-        (self.ysp() as f64 / (self.yw() as f64 / self.xw() as f64)
-         * 0.444_f64).round() as i32
+    fn xsp(&self) -> f64 {
+        self.ysp() / (self.yw() / self.xw()) * 0.444_f64
     }
 
-    fn add_trace(&self, x: i32, y: i32) {
+    fn add_trace(&self, now: &SystemTime, x: i32, y: i32) {
         let mut traces = self.traces.borrow_mut();
 
         for t in traces.iter_mut() {
             if t.p.x == x && t.p.y == y {
-                t.when = SystemTime::now();
+                t.when = *now;
                 return;
             }
         }
 
         traces.push(Trace {
             p: Point { x, y, },
-            when: SystemTime::now(),
+            when: *now,
         });
     }
 }
 
-fn find_scalemax(objects: &[Object]) -> i32 {
+fn find_scalemax(objects: &[Object]) -> f64 {
     let mut xmax = 0_f64;
     let mut ymax = 0_f64;
 
     for o in objects {
-        let x = o.x as f64;
-        let y = o.y as f64;
+        let x = o.x;
+        let y = o.y;
 
         xmax = xmax.max(x.abs() / 2.0).round();
         ymax = ymax.max(y.abs() / 2.0).round();
     }
 
-    ymax.max(xmax / 0.6).round() as i32
+    ymax.max(xmax / 0.6).round()
 }
 
 fn space_to_screen(s: &Space, x: f64, y: f64) -> Point {
-    let x = s.xc() as f64 + (s.xw() as f64 / 2.0) * (x / s.xsp() as f64);
-    let y = s.yc() as f64 + (s.yw() as f64 / 2.0) * (y / s.ysp() as f64);
+    let x = s.xc() + (s.xw() / 2.0) * (x / s.xsp());
+    let y = s.yc() + (s.yw() / 2.0) * (y / s.ysp());
 
-    let x = x as i32;
-    let y = y as i32;
+    let x = x.round() as i32;
+    let y = y.round() as i32;
 
     Point { x, y, }
 }
 
-fn draw_body(s: &Space, o: &Object) {
-    let p = space_to_screen(s, o.x, o.y);
-
-    if p.x < s.xmin || p.y < s.ymin || p.x > s.xmax || p.y > s.ymax {
-        return;
+fn draw_space(s: &mut Space, now: &SystemTime, clear: bool) {
+    if !clear {
+        s.term.reset();
+        s.term.draw_box(s.xmin - 1, s.ymin - 1, s.xmax + 1, s.ymax + 1);
+        s.term.moveto(s.xmin - 1 + (s.xmax - s.xmin) / 2 - 5, s.ymin - 1);
+        s.term.write("  S P A C E  ");
     }
 
-    s.term.moveto(p.x, p.y);
-    s.add_trace(p.x, p.y);
-    s.term.fg8(o.colour);
-    s.term.write(&o.name);
-    s.term.reset();
-}
+    let traces = s.traces.borrow();
 
-fn draw_space(s: &mut Space) {
-    let t = &s.term;
-
-    t.clear();
-    t.cursor(false);
-    t.draw_box(s.xmin - 1, s.ymin - 1, s.xmax + 1, s.ymax + 1);
-
-    t.moveto(s.xmin - 1 + (s.xmax - s.xmin) / 2 - 5, s.ymin - 1);
-    t.write("  S P A C E  ");
-
-    {
-        let now = SystemTime::now();
-        let traces = s.traces.borrow();
-
-        for t in traces.iter() {
-            if let Ok(dur) = now.duration_since(t.when) {
-                if dur.as_millis() > (s.settings.tracelife * 1000.0) as u128 {
-                    continue;
+    for t in traces.iter() {
+        if let Ok(dur) = now.duration_since(t.when) {
+            if dur.as_millis() > (s.settings.tracelife * 1000.0) as u128 {
+                if clear {
+                    s.term.moveto(t.p.x, t.p.y);
+                    s.term.write(" ");
                 }
-
-                let intens = (255.0 -
-                    dur.as_millis() as f64 / (s.settings.tracelife * 1000.0) *
-                    (255.0 - 232.0)).floor() as u8;
-
-                s.term.moveto(t.p.x, t.p.y);
-                s.term.fg8(intens);
-                s.term.write("·");
-                s.term.reset();
+                continue;
             }
+
+            if clear {
+                continue;
+            }
+
+            let intens = (255.0 -
+                dur.as_millis() as f64 / (s.settings.tracelife * 1000.0) *
+                (255.0 - 232.0)).floor() as u8;
+
+            s.term.moveto(t.p.x, t.p.y);
+            s.term.fg8(intens);
+            s.term.write("·");
         }
     }
+    s.term.reset();
+    drop(traces);
 
     for o in &s.objects {
-        draw_body(s, &o);
-    }
+        let p = space_to_screen(s, o.x, o.y);
 
-    t.flush();
+        if p.x < s.xmin || p.y < s.ymin || p.x > s.xmax || p.y > s.ymax {
+            continue;
+        }
+
+        s.term.moveto(p.x, p.y);
+        if clear {
+            for _ in 0..o.name.len() {
+                s.term.write(" ");
+            }
+        } else {
+            s.add_trace(now, p.x, p.y);
+            s.term.fg8(o.colour);
+            s.term.write(&o.name);
+        }
+    }
+    s.term.reset();
 }
 
 fn move_things(s: &mut Space) {
@@ -202,7 +205,7 @@ fn move_things(s: &mut Space) {
             let dy = oo.y - o.y;
 
             let theta = dy.atan2(dx);
-            let rsq = dy * dy + dx * dx;
+            let rsq = dy.powi(2) + dx.powi(2);
 
             let Fg = GRAV * o.mass * oo.mass / rsq;
 
@@ -261,6 +264,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let term = ANSITerm::new()?;
     term.alternate();
+    term.clear();
+    term.cursor(false);
 
     let sz = term.size();
     let scalemax = find_scalemax(&objects);
@@ -281,13 +286,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         traces: RefCell::new(Vec::new()),
     };
 
-    for _ in 0..100 {
+    let mut then = SystemTime::now();
+    for _ in 0..10000 {
         for _ in 0..20_000 {
             move_things(&mut space);
         }
-        draw_space(&mut space);
 
-        std::thread::sleep(std::time::Duration::from_millis(50));
+        let now = SystemTime::now();
+        draw_space(&mut space, &then, true);
+        draw_space(&mut space, &now, false);
+        space.term.flush();
+        then = now;
+
+        std::thread::sleep(std::time::Duration::from_millis(10));
     }
 
     space.term.soft_reset();
